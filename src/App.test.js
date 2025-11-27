dace0d5b6c1de0448d56412ccaeb844f946cdac9
iabdiblandon <iabdiblandon@ethz.ch>
2025-11-27 15:17:01 +0100

    adjust test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders app title', () => {
+    render(<App />);
+    const titleElement = screen.getByText(/App Ref. Card 02/i);
+    expect(titleElement).toBeInTheDocument();
 });
